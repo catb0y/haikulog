a9e40d71c11ee534fbea2afc48a5df639e147a65
Marta <mgn.palandri@gmail.com>
2017-10-30 16:00:20 +0100

    Database test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import math
 
 
 # Create SQLAlchemy object
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config['DATABASE_URL'] = 'postgres://dayjxqmgfwirvj:8abcb93a65e01d03d23c9c43e43e74a187fd464c2198a122fbbcd9006d91ce3b@ec2-54-83-40-208.compute-1.amazonaws.com:5432/datd52rju45245
